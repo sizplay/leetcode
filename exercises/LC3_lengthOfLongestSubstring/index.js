499df440fce1ba3e33d76a8070667a95417ecfee
Chaehoon Lim <sizplay@gmail.com>
2019-11-04 23:13:25 +0900

    first push

---
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -0,0 +1,25 @@
+// Given a string, return the length of the longest substring without
+// repeating characters.
+// --- Example
+// lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
+// lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
+
+function lengthOfLongestSubstring(word) {
+  const map = {};
+  let start = 0;
+  let max = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    const endChar = word[i];
+
+    if (map[endChar] >= start) {
+      start = map[endChar] + 1;
+    }
+
+    map[endChar] = i;
+    max = Math.max(max, i - start + 1);
+  }
+  return max;
+}
+
+module.exports = lengthOfLongestSubstring;
